4edc6bd2236e818026e05dfdec37fe5beca1dda9
Pablo <pavel.konstantinov1@mail.ru>
2023-04-29 23:33:46 +0800

    reactPWA via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,59 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
+
+const vitePWA = VitePWA({
+  registerType: "autoUpdate", 
+  outDir: "build", 
+  manifest: {
+    name: 'Garantex test task', 
+    short_name: 'garantex task', 
+    description: "test task reactPWA", 
+    theme_color: "#ffffff", 
+    icons: [{
+      src: 'assets/images/android-chrome-192x192.png', 
+      sizes: '192x192', 
+      type: 'image/png'
+    }, 
+    {
+      src: 'assets/images/android-chrome-384x384.png', 
+      sizes: '384x384', 
+      type: 'image/png'
+    }]
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: "es2017",
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.(ttf|otf|fnt|woff)$/.test(name ?? "")) {
+            return "assets/fonts/[name]-[hash][extname]";
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: true,
+  },
+  plugins: [react(), vitePWA],
 })
+
+
